381b19f6eef393397314ff0d2a741bbbcbfe81f7
Hamad Khan <hamad.khan@slu.edu>
2024-07-21 00:36:26 -0700

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from 'react';
 import './App.css';
 import TypingTest from './components/TypingTest';
@@ -8,8 +9,34 @@ function App() {
   return (
     <div className={`App theme-${theme}`}>
       <TypingTest />
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> f9e6f21 (Initialize project using Create React App)
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default App;
+=======
+export default App;
+>>>>>>> f9e6f21 (Initialize project using Create React App)
