97dc8e22705201fdc2e0f9d3a10746d94a8f83ba
Hamad Khan <hamoood@amazon.com>
2024-07-23 20:12:29 -0700

    GA4 update.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './styles/App.css';
 import TypingTest from './components/TypingTest';
 import DynamicBackground from './components/DynamicBackground';
-import ReactGA from 'react-ga4';
-
-const MEASUREMENT_ID = 'G-BJ6VGF3MET';
 
 function App() {
-    useEffect(() => {
-        ReactGA.initialize(MEASUREMENT_ID);
-        ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-    }, []);
-
     return (
         <div className="App">
             <DynamicBackground />
